654a4e2f9a71d9ff50e4be3a6e2949d4f05c0dce
mlcsmits <161313142+mlcsmits@users.noreply.github.com>
2024-05-27 11:07:09 +0200

    Delete NW_exons.py

--- NW_exons.py
+++
@@ -1,7 +0,0 @@
-with open("/mnt/studentfiles/2024/2024MBI03/NW_exons.txt", "r") as NW:
-    NW_lines = NW.readlines()[1:]
-    for line in NW_lines:
-        column = line.split('\t')
-        if column[1] == 'exon':
-            with open("/mnt/studentfiles/2024/2024MBI03/NW_exons_filtered.txt", "a") as filt:
-                filt.write(line)
